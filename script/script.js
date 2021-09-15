71419955788ace99ccdf7057982a4d626d08babb
Nick White <bbycinka@gmail.com>
2021-09-15 20:36:59 +0300

    lesson 9

--- script/script.js
+++ script/script.js
@@ -1,3 +1,55 @@
 "use strict";
 
-let number = 10;
+// let num = 4.6;
+
+// console.log(4/0);
+
+// console.log('hello'* 9);
+
+
+// const Person = `Alex`;
+
+// const bool = true;
+
+// let something;
+
+// console.log(something);
+
+// const obj = {
+//     name: 'Nik',
+//     age: 20,
+//     isMarried: false
+// };
+
+// const result = confirm('Are you here');
+// console.log(result);
+
+// const answer = prompt('Вам 18 лет?','');
+// console.log(answer);
+
+// const answers = [];
+// answers[0] = prompt('Как Ваше имя:','');
+// answers[1] = prompt('Как Ваша фамилия:','');
+// answers[2] = prompt('Сколько Вам лет:','');
+
+// console.log(typeof(answers));
+
+// const category = 'toys';
+// const user = 'Nik';
+
+// alert(`Hi!${user}`);
+
+
+let incr = 10,
+    decr = 10;
+
+// incr++;
+// decr--;
+
+// alert(incr);
+// alert(decr);
+
+const isChecked = true,
+      isClose = false;
+
+console.log( !isClose );
