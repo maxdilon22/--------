b2782cf77dd993f30fff366bc0a64867b1d765c3
Nick White <bbycinka@gmail.com>
2021-09-15 20:47:03 +0300

    first practic

--- script/script.js
+++ script/script.js
@@ -1,55 +1,21 @@
 "use strict";
 
-// let num = 4.6;
+const numberOfFilms = +prompt('Сколько фильмов Вы уже посмотрели?','');
 
-// console.log(4/0);
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies : {},
+    actors : {},
+    genres : [],
+    privat : false
+};
 
-// console.log('hello'* 9);
+const a = prompt('Какой последний фильм Вы посмотрели?','');
+const b = prompt('На сколько вы оцените его?','');
+const c = prompt('Какой последний фильм Вы посмотрели?','');
+const d = prompt('На сколько вы оцените его?','');
 
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
 
-// const Person = `Alex`;
-
-// const bool = true;
-
-// let something;
-
-// console.log(something);
-
-// const obj = {
-//     name: 'Nik',
-//     age: 20,
-//     isMarried: false
-// };
-
-// const result = confirm('Are you here');
-// console.log(result);
-
-// const answer = prompt('Вам 18 лет?','');
-// console.log(answer);
-
-// const answers = [];
-// answers[0] = prompt('Как Ваше имя:','');
-// answers[1] = prompt('Как Ваша фамилия:','');
-// answers[2] = prompt('Сколько Вам лет:','');
-
-// console.log(typeof(answers));
-
-// const category = 'toys';
-// const user = 'Nik';
-
-// alert(`Hi!${user}`);
-
-
-let incr = 10,
-    decr = 10;
-
-// incr++;
-// decr--;
-
-// alert(incr);
-// alert(decr);
-
-const isChecked = true,
-      isClose = false;
-
-console.log( !isClose );
+console.log(personalMovieDB);
