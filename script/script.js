ec72673616c1efe14cf04f676b3719e74006d2a3
Nick White <bbycinka@gmail.com>
2021-10-02 10:38:31 +0300

    урок методы и свойства строк и чисел

--- script/script.js
+++ script/script.js
@@ -1,40 +1,23 @@
 "use strict";
 
-// const num = 10;
-// function showFirstMsg() {
-//     let num = 10;
-//     return num;
-// }
 
-// console.log(showFirstMsg());
+// const str = 'teSt';
 
-// function calc(a,b) {
-//     return a +b;
-// }
-
-// console.log(calc(4,6));
-// console.log(calc(4,4));
-// console.log(calc(1,6));
+// console.log(str.indexOf('t'));
 
 
-// function ret(){
-//     let num = 100;
-//     return num;
-// }
-
-// let one = ret();
-
-// console.log(one);
+// const logg = 'Hello world';
 
 
-// let two = function(){
-//     return console.log('hello');
-// };
-// two();
+// console.log(logg.substring(6,11));
 
-// const calc = (a,b) => {
-//     console.log('1');
-//     return a + b;
-// };
+// console.log(logg.substr(6,5));
 
-// console.log(1,4);
+
+const num = 12.2;
+
+console.log(Math.round(num));
+
+const test = '12.2px';
+
+console.log(parseFloat(test));
