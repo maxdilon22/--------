72cc1dc7e891f8d94719516aa092cc812ebf31f6
Nick White <bbycinka@gmail.com>
2021-10-03 14:07:42 +0300

    объекты

--- script/script.js
+++ script/script.js
@@ -1,16 +1,46 @@
 "use strict";
 
-// (function(){
-//     alert('hello');
-// }());
+const options = {
+    name: 'test',
+    width: 1024,
+    height: 1024,
+    colors: {
+        border: 'black',
+        background: 'red'
+    },
+    makeTest: function(){
+        console.log('test');
+    }
+};
 
-function first() {
-    console.log('1');
-}
 
-function learnJS(lang, callback) {
-    console.log(`Я учу ${lang}`);
-    callback();
-}
+// console.log(options.name);
 
-learnJS('JS', () => console.log('я прошел этот урок!'));
+// delete options.name;
+
+// console.log(options);
+
+// let counter = 0;
+
+// for (let key in options) {
+//     if (typeof(options[key]) === 'object') {
+//         for (let i in options[key]) {
+//             console.log(`Свойство ${i} имеет значение ${options[key][i]}`);
+//             }
+//         } else {
+//             console.log(`Свойство ${key} имеет значение ${options[key]}`);
+//             counter++;
+//     }
+// }
+
+// console.log(counter);
+
+// const {border,background} = options.colors;
+// console.log(border);
+
+// // console.log(Object.keys(options).length);
+// // options.makeTest();
+
+
+
+const {border,background} = options.colors;
