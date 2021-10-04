3a8647c924fd51c4a6a8d60de5e9e8844be2600c
Nick White <bbycinka@gmail.com>
2021-10-04 20:26:08 +0300

    Поверхностные ссылки объектов и массивов

--- script/script.js
+++ script/script.js
@@ -1,34 +1,96 @@
 "use strict";
 
-const arr = [1,32,35,6,8];
+// let a = 5,
+//     b = a;
+// b = b + 5;
 
-arr.sort( (a,b) => {
-    return a - b;
-});
+// console.log(b);
+// console.log(a);
 
-console.log(arr);
+const obj = {
+    a: 5,
+    b: 1
+};
 
-// arr[10] = 0;
-// console.log(arr);
-// arr.pop();
-// console.log(arr);
-// arr.push(10);
-// console.log(arr);
+// const copy = obj;
 
-// for (let i = 0; i < arr.length; i++) {
-//     console.log(arr[i]);
-// }
+// copy.a = 10;
 
-// for (let value of arr) {
-//     console.log(value);
-// }
+// console.log(copy);
+// console.log(obj);
 
-// arr.forEach( (item,i,arr) => {
-//     console.log(`${i}: ${item} внутри массива ${arr}`);
-// } );
+function copy(mainObj) {
+    let objCopy = {};
+
+    let key;
+    for (key in mainObj) {
+        objCopy[key] = mainObj[key];
+    }
+    return objCopy;
+}
+
+const numbers = {
+    a: 2,
+    b: 5,
+    c: {
+        x: 7,
+        y: 4
+    }
+};
+
+// const newNumbers = copy(numbers);
+// newNumbers.a = 10;
+// newNumbers.c.x = 10;
+// console.log(newNumbers);
+// console.log(numbers);
 
 
-// const str = prompt('','');
-// const products = str.split(', ');
-// products.sort();
-// console.log(products.join('; '));
+// const add = {
+//     d: 17,
+//     e: 20
+// };
+
+// const clone = Object.assign({},add);
+// clone.d = 20;
+// console.log(add);
+// console.log(clone);
+
+
+// const oldArray = ['a','b','c'];
+// const newArray = oldArray.slice();
+// newArray[1] = 'this is good';
+// console.log(newArray);
+// console.log(oldArray);
+
+const video = ['youtube','vimeo','rutube'];
+const blogs = ['wordpress','blogger','journal'];
+const internet = [...video,...blogs,'vk','facebook'];
+
+console.log(internet);
+
+
+function log(a,b,c) {
+    console.log(a);
+    console.log(b);
+    console.log(c);
+}
+
+const num = [2,5,7];
+
+log(...num);
+
+
+const array = ['a','b']; 
+
+const neArray = [...array];
+
+const q = {
+    one: 1,
+    two: 2,
+    three: 3
+};
+
+
+const newobj = {...q};
+
+console.log(newobj);
