3f421708df87bbd011f8a48c80f78168b8ba63c0
Nick White <bbycinka@gmail.com>
2021-10-04 21:09:20 +0300

    ООП

--- script/script.js
+++ script/script.js
@@ -1,96 +1,21 @@
 "use strict";
 
-// let a = 5,
-//     b = a;
-// b = b + 5;
-
-// console.log(b);
-// console.log(a);
-
-const obj = {
-    a: 5,
-    b: 1
-};
-
-// const copy = obj;
-
-// copy.a = 10;
-
-// console.log(copy);
-// console.log(obj);
-
-function copy(mainObj) {
-    let objCopy = {};
-
-    let key;
-    for (key in mainObj) {
-        objCopy[key] = mainObj[key];
-    }
-    return objCopy;
-}
-
-const numbers = {
-    a: 2,
-    b: 5,
-    c: {
-        x: 7,
-        y: 4
+const solider = {
+    health: 400,
+    armor: 100,
+    sayHello: function() {
+        console.log('hi!');
     }
 };
 
-// const newNumbers = copy(numbers);
-// newNumbers.a = 10;
-// newNumbers.c.x = 10;
-// console.log(newNumbers);
-// console.log(numbers);
+const John = Object.create(solider);
 
+// const John = {
+//     health: 100
+// }
 
-// const add = {
-//     d: 17,
-//     e: 20
-// };
+// John.__proto__ = solider;
 
-// const clone = Object.assign({},add);
-// clone.d = 20;
-// console.log(add);
-// console.log(clone);
+// Object.setPrototypeOf(John,solider);
 
-
-// const oldArray = ['a','b','c'];
-// const newArray = oldArray.slice();
-// newArray[1] = 'this is good';
-// console.log(newArray);
-// console.log(oldArray);
-
-const video = ['youtube','vimeo','rutube'];
-const blogs = ['wordpress','blogger','journal'];
-const internet = [...video,...blogs,'vk','facebook'];
-
-console.log(internet);
-
-
-function log(a,b,c) {
-    console.log(a);
-    console.log(b);
-    console.log(c);
-}
-
-const num = [2,5,7];
-
-log(...num);
-
-
-const array = ['a','b']; 
-
-const neArray = [...array];
-
-const q = {
-    one: 1,
-    two: 2,
-    three: 3
-};
-
-
-const newobj = {...q};
-
-console.log(newobj);
+console.log(John.sayHello());
