1c023e26668b0c9eb9501cfd8067bac24f442e10
Nick White <bbycinka@gmail.com>
2021-10-05 20:28:23 +0300

    Действия с элементами на странице

--- script/script.js
+++ script/script.js
@@ -1,20 +1,51 @@
 "use strict";
-
+const wrapper = document.querySelector('.wrapper');
 const box = document.getElementById('box');
 const btns = document.getElementsByTagName('button');
-console.log(box);
-console.log(btns[1]);
-
 const cyrcles = document.getElementsByClassName('circle');
+const hearths = wrapper.querySelectorAll('.hearth');
+const oneHearth = wrapper.querySelector('.hearth');
 
-console.log(cyrcles[0]);
+// box.style.backgroundColor = 'blue';
+// box.style.width = '700px';
 
-const hearths = document.querySelectorAll('.hearth');
+btns[1].style.borderRadius = '100%';
 
-hearths.forEach(item => {
-    console.log(item);
-});
+// cyrcles[0].style.backgroundColor = 'red';
 
-const oneHearth = document.querySelector('.hearth');
+// box.style.cssText = 'background-color: blue; width: 500px';
 
-console.log(oneHearth);
+// for (let i = 0; i < hearths.length; i++) {
+//     hearths[i].style.backgroundColor = 'red';
+// }
+
+// hearths.forEach( (item) => {
+//     item.style.backgroundColor = 'red';
+// });
+
+// for (let key of hearths) {
+//     key.style.backgroundColor = 'red';
+// }
+
+const div = document.createElement('div');
+div.innerHTML = '<h1>Hello, world!</h1>';
+// div.insertAdjacentHTML('afterend','<h2>Hello</h2>');
+// div.textContent = 'hello';
+// const txt = document.createTextNode('hi!');
+
+div.classList.add('black');
+
+wrapper.prepend(div);
+// hearths[0].after(div);
+// cyrcles[0].remove();
+
+// hearths[0].replaceWith(hearths[1]);
+// Старые методы
+// wrapper.appendChild(div);
+
+// wrapper.insertBefore(div, hearths[1]);
+
+
+// wrapper.removeChild(hearths[0]);
+
+// wrapper.replaceChild(div,hearths[0]);
