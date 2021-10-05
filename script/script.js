2034a7f66e20f0c5edc6dc67892283645ab09f05
Nick White <bbycinka@gmail.com>
2021-10-05 18:58:59 +0300

    Получение элементов со страницы

--- script/script.js
+++ script/script.js
@@ -1,35 +1,20 @@
 "use strict";
 
-console.log(String(null));
+const box = document.getElementById('box');
+const btns = document.getElementsByTagName('button');
+console.log(box);
+console.log(btns[1]);
 
-const num = 5;
+const cyrcles = document.getElementsByClassName('circle');
 
-console.log("https://vk.com/catalog/" + num);
+console.log(cyrcles[0]);
 
-const fontSize = 26 + 'px';
+const hearths = document.querySelectorAll('.hearth');
 
+hearths.forEach(item => {
+    console.log(item);
+});
 
+const oneHearth = document.querySelector('.hearth');
 
-console.log(Number('24'));
-
-
-// let answer = +prompt('What is your name: ','');
-
-// console.log(typeof(answer));
-
-
-//Boolean
-
-// 0, '', undefined, null, NaN
-
-let switcher = null;
-switcher = 1;
-if (switcher) {
-    console.log('ok!');
-} else {
-    console.log('error!');
-}
-
-
-console.log(typeof(Boolean('4')));
-console.log(typeof(!!'4'));
+console.log(oneHearth);
