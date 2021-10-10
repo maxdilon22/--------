4f3c1ed01bda934cfa9eb192308f942ecd630783
Nick White <bbycinka@gmail.com>
2021-10-10 14:43:14 +0300

    Функции контрукторы

--- script/script.js
+++ script/script.js
@@ -1,28 +1,21 @@
 'use strict';
+function User(name,id) {
+    this.name = name;
+    this.id = id;
+    this.human = true;
+    this.hello = function () {
+        console.log(`hello! ${this.name}`);
+    };
+}
+User.prototype.exit = function(name) {
+    console.log(`Пользователь ${this.name} ушел!`);
+};
+const nick = new User('Nick',20);
+console.log(nick);
 
-const box = document.querySelector('.box');
-const btn = document.querySelector('button');
-
-// const width = box.clientWidth;
-// const height = box.clientHeight;
-
-// const width = box.offsetWidth;
-// const height = box.offsetHeight;
-
-const width = box.scrollWidth;
-const height = box.scrollHeight;
-
-console.log(width, height);
-
-btn.addEventListener('click',()=> {
-    // box.style.height = box.scrollHeight + 'px';
-    console.log(box.scrollTop);
-});
+nick.hello();
 
 
-console.log(box.getBoundingClientRect());
 
-const style = window.getComputedStyle(box);
+nick.exit();
 
-
-console.log(style.display);
