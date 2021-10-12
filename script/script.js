fcc90450edc0ab9c7ffed4193642c63d3c318c42
Nick White <bbycinka@gmail.com>
2021-10-12 19:45:50 +0300

    Классы ES6

--- script/script.js
+++ script/script.js
@@ -1,109 +1,29 @@
 'use strict';
 
-// function showThis(a,b) {
-//     console.log(this);
-//     function sum() {
-//        return a + b;
-//     }
-// }
-
-// showThis();
-
-// console.log(sum());
-// showThis(4,5);
-
-// Обычная фукнция возвращает window а асли use strict --> underfined
-// Контекст this у объектов это объект
-// bind, apply объект[Аргумент], call(объект,Аргумент)
+class Rectangle {
+    constructor(height, width) {
+        this.height = height;
+        this.width = width;
+    }
+    calcArea() {
+        return this.height * this.width;
+    }
+}
 
 
-// const obj = {
-//     a: 20,
-//     b: 15,
-//     sum: function() {
-//         function shout() {
-//             console.log(this);
-//         }
-//         shout();
-//     }
-// };
+class ColoredRectangleWithText extends Rectangle {
+    constructor(width, height, text, bgColor) {
+        super(height,width);
+        this.text = text;
+        this.bgColor = bgColor;
+    }
+    showMyProps() {
+        console.log(`Текст: ${this.text}, цвет: ${this.bgColor}`);
+    }
+}
 
-// console.log(obj.sum);
-
-// function User(name, id) {
-//     this.name = name;
-//     this.id = id;
-//     this.human = true;
-
-// }
-
-// let nick = new User('nick',20);
-
-// function sayName(surname) {
-//     console.log(this);
-//     console.log(this.name + surname);
-// }
-
-// const user = {
-//     name: 'John'
-// };
-
-// sayName.call(user, 'Smith');
-// sayName.apply(user, ['Smith']);
-
-
-// function count(num) {
-//     return this * num;
-// }
-
-// const double = count.bind(2);
-
-// console.log(double(2));
-
-// const btn = document.querySelector('button');
-
-// // btn.addEventListener('click', function() {
-// //     console.log(this.style.background = 'red');
-// // });
-
-// btn.addEventListener('click', e => {
-//     console.log(e.target.style.background = 'red');
-// })
-
-// // const obj = {
-// //     num: 5,
-// //     sayNumber: function() {
-// //         const say = () => console.log(this);
-// //     }
-// // };
-
-// // obj.sayNumber();
-
-// const double = a =>  a * 2;
-
-// console.log(double(4));
-
-
-// function say() {
-//     return console.log(this);
-// }
-
-// const obj = {
-//     name: 'Nick'
-// };
-
-// say.call(obj);
-
-
-// const usrName = say.bind('Nick');
-
-// console.log(usrName('Kh'));
-
-
-
-// const obj = {
-//     name: 'nick',
-//     say: function() {
-//         () => console.log(this);
-//     }
-// };
+const square = new Rectangle(10,10),
+      long = new Rectangle(20,100),
+      div = new ColoredRectangleWithText(25,10,'salam','#fff');
+div.showMyProps();
+console.log(div.calcArea());
